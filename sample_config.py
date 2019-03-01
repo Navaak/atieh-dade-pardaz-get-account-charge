7db1698c7009ab84e31138cca75ef45cb8878b89
Shiva Chavoshian <chavoshian.shiva@gmail.com>
2019-03-01 20:45:26 +0330

    add sample config file

---
+++ sample_config.py
@@ -0,0 +1,9 @@
+
+### This is a config file in json foramt for user information
+
+mobile_number_list = ['0','0','0']
+username = 'your_user_name'
+password = 'your_password'
+srcaddress = 'your_code'
+charge_amount = 'the amount that you want to get warning alert'
+charge_critical_amount = 'the amount that you want to get critical alert'
